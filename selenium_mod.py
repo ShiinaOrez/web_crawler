73cb2b8a54847f9b3225192cf1e734707d8be74b
ShiinaOrez <shiina_orez@qq.com>
2017-12-15 15:26:27 +0800

    2017.12.15

--- selenium_mod.py
+++ selenium_mod.py
@@ -1,12 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver=webdriver.PhantomJS(executable_path='/usr/local/bin/phantomjs')
-driver.get("http://www.python.org")
-assert "Pyhton" in driver.title
-elem=driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+driver=webdriver.Chrome()
+
+driver.get("http://github.com/ShiinaOrez")
+
+shiina_id=driver.find_element_by_id()
+
+print (shiina_id)
+
 driver.close()
--- urllib3_test.py
+++ urllib3_test.py
@@ -1,11 +1,19 @@
 from urllib import request
 
-import json
+#import json
+
+import certifi
 
 import urllib3
 
-http=urllib3.PoolManager()
+http=urllib3.PoolManager(cert_reqs="CERT_REQUIRED",timeout=10.0,ca_certs=certifi.where())
 #实例化PoolManager
+'''
+#ProxyManager
+proxy=urllib3.ProxyManager("http://localhost:3128")
+
+proxy.request("GET","http:/google.com/")
+'''
 '''
 proxy=request.ProxyHandler({"http":"localhost:9999"})
 
@@ -33,8 +41,29 @@ encoded_data=json.dumps(data).encode('utf-8')
 response-http.request('POST','http://httpbin.org/post',body=encoded_data,headers={'Content-Type':'application/json'})
 #将需要发送的json数据编码后传递给request()的body参数，然后在请求头中设置Content-Type字段为application/json
 '''
-response=http.request('GET',"http://www.baidu.com",headers={'X-something':'value'})
-#调用request发起请求，返回一个HttpResponse对象,并且设置请求头
+'''
+with open('example.txt') as fp:
+	
+	file_data=fp.read()
+
+response=http.request('POST','http://httpbin.org/post',fields={'filefield':('example.txt',file_data,'text/plain')})
+'''
+'''
+#流式处理大额响应：
+requesthttp.request("GET","http://httpbin.org/bytes/1024",preload_content=False)
+
+for chunk in r.stream(32):
+	print(chunk)
+response.release_conn()
+#释放本次链接
+'''
+try:
+
+	response=http.request('GET',"http://www.baidu.com",headers={'X-something':'value'})	
+	#调用request发起请求，返回一个HttpResponse对象,并且设置请求头
+except urllib3.exceptions.NewConnectorError as f:
+	print ("Connection failed !",f)
+
 header=response.headers
 #响应头
 h=str(header)
@@ -50,11 +79,8 @@ d=str(data)
 with open("baidu/baidu_headers.txt","w") as f:
 	f.write(h)
 
-#f=open("baidu/baidu_status.txt","w")
-
-#f.wirte(s)
-
-#close(f)
+with open("baidu/baidu_status.txt","w") as f:
+	f.write(s)
 
 with open("baidu/baidu_data.html","w") as f:
 	f.write(d)
