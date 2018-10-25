ef0c27bb1e1aff684da26c88213db60e8de8ef62
S?O(CCNU) <31346916+ShiinaOrez@users.noreply.github.com>
2018-10-25 19:44:46 +0800

    Update artstation_win.py

--- ArtStation/artstation_win.py
+++ ArtStation/artstation_win.py
@@ -6,7 +6,7 @@ basedir=os.path.abspath(os.path.dirname(__file__))
 
 url = input("The illustor you want to know?[URL]")
 illustor_name = url.replace('https://www.artstation.com/','')
-j = 'https://www.artstation.com/users/' + illustor_name + '/projects.json?page=1'
+j = 'https://www.artstation.com/users/' + illustor_name + '/projects.json?page='
 
 project_url = "https://www.artstation.com/projects/"
 
@@ -20,12 +20,22 @@ headers = {
 }
 
 session = requests.Session()
+data = list([])
 
-response = session.get(j, headers = headers)
+response = session.get(j+"1", headers = headers)
 count = response.json().get('total_count')
+pages = count//50
+if count%50 != 0:
+    pages+=1
 print (illustor_name+" have "+ str(count) + ' illusts')
 number = int(input('how many illusts you want to download to your computer?'))
-data = response.json().get('data')
+for i in range(pages):
+    response = session.get(j+str(i+1), headers = headers)
+    if response.json().get('data') is None:
+        continue
+    for re in response.json().get('data'):
+        data.append(re)
+        
 tot = 1
 for illust in data:
     if tot > number:
@@ -43,32 +53,30 @@ for illust in data:
         response = session.get(assert_json_url, headers = headers)
         for image in response.json().get('assets'):
             large_url = image.get('image_url')
-            if image.get('has_embedded_player') is not False:
-                isImage = False
-            downloadList.append(image.get('image_url'))
-        if isImage:
-            file_path=basedir+'\\Images\\'+illustor_name+'\\'
-            if not os.path.exists(file_path):
-                os.makedirs(file_path)
-            num=-1
-            for downloadLink in downloadList:
-                num+=1
-                headers['referer'] = illust.get('permalink')
-                name = illust.get('title')+illust.get('hash_id')
-                tail = '.png'
-                if 'jpg' in large_url:
-                    tail = '.jpg'
-                file_name=file_path+name+hash_id+'_'+str(num)+tail
-                file_name = file_name.replace('"','')
-                file_name = file_name.replace('/','')
-                file_name = file_name.replace('"','')
-#                print(file_name)
-                if os.path.isfile(file_name): 
-                    print ('>Image already exist = =')
-                    continue
+            if image.get('has_embedded_player') is False:
+                downloadList.append(image.get('image_url'))
+        file_path=basedir+'\\Images\\'+illustor_name+'\\'
+        if not os.path.exists(file_path):
+            os.makedirs(file_path)
+        num=-1
+        for downloadLink in downloadList:
+            num+=1
+            headers['referer'] = illust.get('permalink')
+            name = illust.get('title')+illust.get('hash_id')
+            tail = '.png'
+            if 'jpg' in downloadLink:
+                tail = '.jpg'
+            if '.gif' in downloadLink:
+                tail = '.gif'
+            file_name=file_path+name+hash_id+'_'+str(num)+tail
+            file_name = file_name.replace('"','')
+            file_name = file_name.replace('/','')
+            file_name = file_name.replace('"','')
+#               print(file_name)
+            if os.path.isfile(file_name): 
+                print ('>Image already exist = =')
+                continue
 
-                r=requests.get(downloadLink, headers=headers, allow_redirects=True)
-                open(file_name, 'wb').write(r.content)
-                print (name+hash_id+'_'+str(num)+tail+' download successfully!')
-        else:
-            print("Video!!!!!!!!!Go next!")
+            r=requests.get(downloadLink, headers=headers, allow_redirects=True)
+            open(file_name, 'wb').write(r.content)
+            print (name+hash_id+'_'+str(num)+tail+' download successfully!')
