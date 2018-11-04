07ae370a27ff169852902a967ecb98866646e6df
ShiinaOrez <shiina_orez@qq.com>
2018-11-04 15:43:54 +0800

    update: csdn.py

--- CSDN/csdn.py
+++ CSDN/csdn.py
@@ -4,31 +4,44 @@ from bs4 import BeautifulSoup
 
 url_prefix="https://blog.csdn.net/"
 url_rest="/article/list/"
-
 writer_name=input("input the writer_name:")
-
 url=url_prefix+writer_name+url_rest
-
 f=open(writer_name+'_articles.txt','w')
 
+# get the page 1-5
 for i in range(1,5):
+    # get the page response
     blog=requests.get(url+str(i))
+    # print the status code, 200: OK
     print (blog.status_code)
+    # blog_text is the string
     blog_text=blog.text
     s=BeautifulSoup(blog_text,'html.parser')
-#    print (blog_text)
+    # for all of the <div class = "article-item-box csdn-tracking-sttistics">
     for arti in s.find_all('div',class_='article-item-box csdn-tracking-statistics'):
+        # delete the <div style = "xxx">
         if arti.attrs.get('style') is not None:
             continue
+        # write something
         f.write("""
 =========="""+'\n')
+        """
+        <div>
+          <a href = "https://......"> ... </a>
+        </div>
+        """
         f.writelines('the article url:'+str(arti.a.attrs.get('href'))+'\n')
         title=str(arti.a.text)
-        """span=arti.a.h3.span
-        if span is not None :
-            title=title[16:]
-        else: title=title[7:] """
         f.writelines('the article title:'+str(title)+'\n')
+        """
+        <div>
+          <div>
+            <p> <span> ... </span> 1 </p>
+            <p> <span> ... </span> 2 </p>
+            ...
+          </div>
+        </div>
+        """
         for div in arti.div.find_all('p'):
             if div.span is not None:
                 if 'read-num' in div.span.attrs.get('class') :
